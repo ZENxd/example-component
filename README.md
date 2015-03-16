b06894fde05cfe463710aaa00150b9a66be50a58
Dave Stewart <dave@metanurb.com>
2015-03-16 15:28:32 -0400

    Initial commit of templates

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # example-component
-Example Angular templates for DG demo component
+Example Angular templates for DG demo component.
+
+
+Use the templates to build out your controller, directives, html and css.
+NOTE: Do not change the module or controller names.
---
+++ example.js
@@ -0,0 +1,20 @@
+(function() {
+	'use strict';
+	angular.module('myModule', [])
+	.controller('myController', ['$scope', '$compile', function($scope, $compile) {
+		
+		//Controller vars & methods needed for demo
+		$scope.hello = 'Hello World!';
+
+
+	}])
+	.directive('hello', function(){
+		return {
+			restrict: 'E',
+			scope: {
+					data:'@'
+			},
+			template:	'<div><h3>{{data}}</h3></div>'
+		};
+	});
+})();
